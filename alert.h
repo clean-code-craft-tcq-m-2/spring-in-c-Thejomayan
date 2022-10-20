9703d69ee4d9574219bc8c3d14b5820066b6f3da
Thejomayan <115685391+Thejomayan@users.noreply.github.com>
2022-10-20 16:16:44 +0530

    Update alert.h

--- alert.h
+++ alert.h
@@ -2,9 +2,9 @@
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
 
-void emailAlerter(void);
-void ledAlerter(void);
+extern void check_and_alert(float maxThreshold, alerter_funcptr alert_array[], Stats_t computedStats);
+extern void emailAlerter(void);
+extern void ledAlerter(void);
 
-void check_and_alert(float maxThreshold, alerter_funcptr alert_array[], Stats_t computedStats);
 
 typedef void (*alerter_funcptr)(void);
