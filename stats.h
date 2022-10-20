9a18489d52dff2f274217b300d6678e2511f78a8
Thejomayan <115685391+Thejomayan@users.noreply.github.com>
2022-10-20 15:41:11 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -7,8 +7,3 @@ typedef struct Stats_s
 
 Stats_t compute_statistics(const float* numberset, int setlength);
 
-typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
-
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
