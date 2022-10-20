6876e5b6b869261309c9b7bc4f86c38a211ee68b
Thejomayan <115685391+Thejomayan@users.noreply.github.com>
2022-10-20 16:10:12 +0530

    Update alert.h

--- alert.h
+++ alert.h
@@ -6,3 +6,5 @@ void emailAlerter(void);
 void ledAlerter(void);
 
 void check_and_alert(float maxThreshold, alerter_funcptr alert_array[], Stats_t computedStats);
+
+typedef void (*alerter_funcptr)(void);
